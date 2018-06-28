6f083d5b09273280be9b9087a8eb5858fe560cbc
ParamosManuver <paramosmanuver@gmail.com>
2018-06-28 19:11:28 +0300

    Vue Syntax: if Conditionals

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="test">
         <h1>{{title}}</h1>
-        <p>{{user.first_name +' '+ user.last_name}}</p>
+        <p v-if="showName">{{user.first_name +' '+ user.last_name}}</p>
+        <p v-else>No one By that name</p>
         <p v-text="user.first_name"></p>
         
         <!-- Parse HTML -->
@@ -22,7 +23,8 @@ export default {
           user:{
               first_name: 'Jake',
               last_name: 'Archibald'
-          }
+          },
+          showName: false
       }
   }
 };
