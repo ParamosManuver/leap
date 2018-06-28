4291f1d774198fed082d2917dcf20c6f0ca2ab4b
ParamosManuver <paramosmanuver@gmail.com>
2018-06-28 19:07:09 +0300

    Some Vue Basics

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,12 +1,30 @@
 <template>
     <div class="test">
-        <h1>Test</h1>
+        <h1>{{title}}</h1>
+        <p>{{user.first_name +' '+ user.last_name}}</p>
+        <p v-text="user.first_name"></p>
+        
+        <!-- Parse HTML -->
+        <!-- Unparsed -->
+        {{day}} 
+        <!-- Parsed -->
+        <span v-html="day"></span>
     </div>
 </template>
 
 <script>
 export default {
-  name: 'Test'
+  name: 'Test',
+  data(){
+      return{
+          title: 'Hello World',
+          day: '<h1>Thursday</h1>',
+          user:{
+              first_name: 'Jake',
+              last_name: 'Archibald'
+          }
+      }
+  }
 };
 </script>
 
