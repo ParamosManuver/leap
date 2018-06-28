f99a256161a80c2041412f93b0ceca992d0306c1
ParamosManuver <paramosmanuver@gmail.com>
2018-06-28 21:40:39 +0300

    Addding more youtubers

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,6 +1,21 @@
 <template>
     <div class="users">
         <h1>Hello, my name is human</h1>
+        <form v-on:submit="addUser">
+          <input type="text" v-model="newUser.name" placeholder="Enter Name">
+          <br>
+          <input type="text" v-model="newUser.email" placeholder="Enter Email">
+          <br>
+          <input type="submit" value="Submit">
+        </form>
+        
+        <ul>
+          <li v-for="user in users">
+            <span>
+              {{user.name}} : {{user.email}}
+            </span>
+          </li>
+        </ul>
     </div>
 </template>
 
@@ -10,6 +25,9 @@ export default {
 
   data() {
     return {
+      newUser: {
+
+      },
       users: [
         {
           name: 'Marquez Brownlee',
@@ -27,9 +45,20 @@ export default {
           contacted: false
         }
       ]
-    };
+    }
+  },
+
+    methods: {
+    addUser: function(event){
+      this.users.push({
+        name: this.newUser.name,
+        email: this.newUser.email,
+        contacted: false
+      });
+      event.preventDefault();
+      }
+    }
   }
-};
 </script>
 
 <style scoped>
