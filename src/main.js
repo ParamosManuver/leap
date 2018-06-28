1fe7e3f4aeea937ea13bf6b9badbc28507bee0c8
ParamosManuver <paramosmanuver@gmail.com>
2018-06-28 22:07:54 +0300

    imported vue-resourse

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+// Vue-Resourse
+import vueResourse from 'vue-resource'
 // Main App Component
 import App from './App'
+
+// TO use other imported modules
+Vue.use(vueResourse)
 
 Vue.config.productionTip = false
 
