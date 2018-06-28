446eac6861fa48006031af03d72e521ade1a049d
ParamosManuver <paramosmanuver@gmail.com>
2018-06-28 18:48:35 +0300

    Added Component Test

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="app">
+    <test></test>
   </div>
 </template>
 
 <script>
- 
+import Test from './components/Test';
+
 // Export defaullt component with name App
 export default {
   name: 'App',
   // Include other components
   components: {
-    
+    Test
   }
 }
 </script>
